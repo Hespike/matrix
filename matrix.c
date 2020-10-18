281901ff9b5b27bf4b9e43b90cd0690ca2e39936
Hespi <sardi.andras@gmail.com>
2020-10-18 21:53:52 +0200

    Add files via upload

---
+++ matrix.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int matrix[3][3] = {{4,5,6},{7,8,9},{20,30,40}};
+
+int i, j;
+for(i = 0; i<3; i++){
+    for(j =0; j < 3; j++){
+        printf("%d ", matrix[i][j]);
+    }
+    printf("\n");
+}
+return 0;
+}
